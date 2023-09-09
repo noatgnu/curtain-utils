3eefc067e045dbb8f24ebee44594f13bf08cda4b
Toan Phung <toan.phung@proteo.info>
2023-09-09 17:09:58 +0100

    update

--- curtainutils/common.py
+++ curtainutils/common.py
@@ -1,6 +1,146 @@
+from typing import List
+
 import pandas as pd
 from uniprotparser.betaparser import UniprotSequence
 
+curtain_base_de_form = {
+    "_reverseFoldChange": False,
+    "_comparisonSelect": [],
+    "_comparison": "",
+    "_primaryIDs": "",
+    "_geneNames": "",
+    "_foldChange": "",
+    "_transformFC": False,
+    "_significant": "",
+    "_transformSignificant": False,
+}
+
+curtain_base_raw_form = {
+    "_primaryIDs": "",
+    "_samples": [],
+}
+
+curtain_base_input_file = {
+    "df": "",
+    "filename": "",
+    "other": {},
+    "originalFile": ""
+}
+
+curtain_base_project_form = {
+    "title": "",
+    "projectDescription": "",
+    "organisms": [{"name": ""}],
+    "organismParts": [{"name": ""}],
+    "cellTypes": [{"name": ""}],
+    "diseases": [{"name": ""}],
+    "sampleProcessingProtocol": "",
+    "dataProcessingProtocol": "",
+    "identifiedPTMStrings": [{"name": ""}],
+    "instruments": [{"name": "", "cvLabel": "MS"}],
+    "msMethods": [{"name": ""}],
+    "projectTags": [{"name": ""}],
+    "quantificationMethods": [{"name": ""}],
+    "species": [{"name": ""}],
+    "sampleAnnotations": {},
+    "_links": {"datasetFtpUrl": {"href": ""}, "files": {"href": ""}, "self": {"href": ""}},
+    "affiliations": [{"name": ""}],
+    "hasLink": False,
+    "authors": [],
+    "accession": "",
+    "softwares": [{"name": ""}],
+}
+
+curtain_base_payload = {
+    "raw": "",
+    "rawForm": {},
+    "differentialForm": {},
+    "processed": "",
+    "password": "",
+    "selections": [],
+    "selectionsMap": {},
+    "selectionsName": [],
+    "settings": {},
+    "fetchUniprot": True,
+    "annotatedData": {},
+    "extraData": {}
+}
+
+curtain_base_settings = {
+    "fetchUniprot": True,
+    "sampleMap": {},
+    "barchartColorMap": {},
+    "pCutoff": 0.05,
+    "log2FCCutoff": 0.6,
+    "description": "",
+    "uniprot": True,
+    "colorMap": {},
+    "backGroundColorGrey": False,
+    "selectedComparison": [],
+    "version": 2,
+    "currentID": "",
+    "fdrCurveText": "",
+    "fdrCurveTextEnable": False,
+    "prideAccession": "",
+    "project": curtain_base_project_form,
+    "sampleOrder": {},
+    "sampleVisible": {},
+    "conditionOrder": [],
+    "volcanoAxis": {
+        "minX": None,
+        "maxX": None,
+        "minY": None,
+        "maxY": None
+    },
+    "textAnnotation": {},
+    "volcanoPlotTitle": "",
+    "visible": {},
+    "defaultColorList": [
+        "#fd7f6f",
+        "#7eb0d5",
+        "#b2e061",
+        "#bd7ebe",
+        "#ffb55a",
+        "#ffee65",
+        "#beb9db",
+        "#fdcce5",
+        "#8bd3c7",
+    ],
+    "scatterPlotMarkerSize": 10,
+    "rankPlotColorMap": {},
+    "rankPlotAnnotation": {},
+    "legendStatus": {},
+    "stringDBColorMap": {
+        "Increase": "#8d0606",
+        "Decrease": "#4f78a4",
+        "In dataset": "#ce8080",
+        "Not in dataset": "#676666"
+    },
+    "interactomeAtlasColorMap": {
+        "Increase": "#a12323",
+        "Decrease": "#16458c",
+        "HI-Union": "rgba(82,110,194,0.96)",
+        "Literature": "rgba(181,151,222,0.96)",
+        "HI-Union and Literature": "rgba(222,178,151,0.96)",
+        "Not found": "rgba(25,128,128,0.96)",
+        "No change": "rgba(47,39,40,0.96)",
+    },
+    "proteomicsDBColor": "#ff7f0e",
+    "networkInteractionSettings": {
+        "Increase": "rgba(220,169,0,0.96)",
+        "Decrease": "rgba(220,0,59,0.96)",
+        "StringDB": "rgb(206,128,128)",
+        "No change": "rgba(47,39,40,0.96)",
+        "Not significant": "rgba(255,255,255,0.96)",
+        "Significant": "rgba(252,107,220,0.96)",
+        "InteractomeAtlas": "rgb(73,73,101)",
+    },
+    "plotFontFamily": "Arial",
+    "networkInteractionData": [],
+    "enrichrGeneRankMap": {},
+    "enrichrRunList": [],
+    "customVolcanoTextCol": ""
+}
 
 def read_fasta(fasta_file: str) -> pd.DataFrame:
     fasta_dict = {}
@@ -20,3 +160,6 @@ def read_fasta(fasta_file: str) -> pd.DataFrame:
             else:
                 fasta_dict[current_acc] += line.strip()
     return pd.DataFrame([[k, fasta_dict[k]] for k in fasta_dict], columns=["Entry", "Sequence"])
+
+def create_curtain_session(de_file: str, raw_file: str, fc_col: str, p_col: str, comp_col: str, comp_select: List[str], primary_id_de_col: str, primary_id_raw_col:str, sample_cols: List[str]):
+    pass
